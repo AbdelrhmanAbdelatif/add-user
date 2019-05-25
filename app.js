ad93c372dc00b879a352e0d47ca5c20e53bd656c
AbdelrhmanAbdelatif <abdo.abdelatif2015@gmail.com>
2019-05-26 00:25:41 +0200

    semi-modified

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 var myApp = angular.module('myApp' , [ ]);
 
 myApp.controller('storeController', function MyController($scope){
-    $scope.show = {};
-    $scope.persons = [
-        {
-            name: 'Abd',
-            age : '29',
-            job : 'frodev'
-            
-        },
-        {
-            name: 'mohd',
-            age : '30',
-            job : 'dev'
-            
-        }
-
-    ];
+    $scope.show = {
+        fName:"",
+        lName:"",
+        lang:"",
+        email:"",
+        account:"",
+        role:""
+    };
+    $scope.persons = [];
+    $scope.addNewUser=false;
+    $scope.edit_mode=false;
 
     $scope.save = function(){
-        $scope.person.push($scope.show);
-        $scope.show = {};
+        if($scope.edit_mode==false){
+            var person = angular.copy($scope.show);
+            $scope.persons.push(person);    
+        }
+        $scope.reset();
     }
+
+    $scope.edit=function(person){
+        $scope.show=person;
+        $scope.addNewUser=true;
+        $scope.edit_mode=true;
+    }
+
+    $scope.reset=function(){
+        $scope.edit_mode=false;
+        $scope.addNewUser=false;
+        $scope.show = {
+            fName:"",
+            lName:"",
+            lang:"",
+            email:"",
+            account:"",
+            role:""
+        };
+    }
+
     });
 
