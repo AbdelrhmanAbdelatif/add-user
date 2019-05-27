d2072cc23f042ad307cbf04d037c315f11d78277
AbdelrhmanAbdelatif <abdo.abdelatif2015@gmail.com>
2019-05-27 13:13:30 +0200

    task

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
-var myApp = angular.module('myApp' , [ ]);
+var myApp = angular.module('myApp', []);
 
-myApp.controller('storeController', function MyController($scope){
+myApp.controller('storeController', function MyController($scope) {
     $scope.show = {
-        fName:"",
-        lName:"",
-        lang:"",
-        email:"",
-        account:"False",
-        role:""
+        fName: "",
+        lName: "",
+        lang: "",
+        email: "",
+        account: "false",
+        role: ""
     };
     $scope.users = [];
-    $scope.addNewUser=false;
-    $scope.edit_mode=false;
+    $scope.addNewUser = false;
+    $scope.edit_mode = false;
 
-    $scope.save = function(){
-        if($scope.edit_mode==false){
+    $scope.save = function () {
+        if ($scope.edit_mode == false) {
             var user = angular.copy($scope.show);
-            $scope.users.push(user);    
+            $scope.users.push(user);
         }
         $scope.reset();
     }
 
-    $scope.edit=function(user){
-        $scope.show=user;
-        $scope.addNewUser=true;
-        $scope.edit_mode=true;
+    $scope.edit = function (user) {
+        $scope.show = user;
+        $scope.addNewUser = true;
+        $scope.edit_mode = true;
     }
 
-    $scope.reset=function(){
-        $scope.edit_mode=false;
-        $scope.addNewUser=false;
+    $scope.reset = function () {
+        $scope.edit_mode = false;
+        $scope.addNewUser = false;
         $scope.show = {
-            fName:"",
-            lName:"",
-            lang:"",
-            email:"",
-            account:"False",
-            role:""
+            fName: "",
+            lName: "",
+            lang: "",
+            email: "",
+            account: "false",
+            role: ""
         };
     }
 
-    });
+});
 
